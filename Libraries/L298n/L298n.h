b73b6d2f816c2e8b56385848d23664516b726b0d
opentec <ssommers22@yahoo.com>
2017-11-03 13:09:41 -0400

    Started header class format

--- Libraries/L298n/L298n.h
+++ Libraries/L298n/L298n.h
@@ -2,3 +2,22 @@
 	FILE: L298n.h
 	PURPOSE: L298n Motor controller library for Arduino
 */
+
+#ifndef L298n_h
+#define L298n_h
+
+#include "Arduino.h"
+
+class L298n{
+  public:
+	L298n(int enA, int enB, int in1, int in2, int in3, int in4);
+	void stop();
+  private:
+    int _enA;
+	int _enB;
+	int _in1;
+	int _in2;
+	int _in3;
+	int _in4;
+};
+#endif;
