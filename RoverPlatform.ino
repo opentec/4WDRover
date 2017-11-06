08d7623a7f3c643e7891ff20e0cf871d2fb309cd
opentec <ssommers22@yahoo.com>
2017-11-05 19:32:32 -0500

    Added L298n drafted library to main sketch

--- RoverPlatform.ino
+++ RoverPlatform.ino
@@ -1,3 +1,4 @@
+#include <L298n.h>
 // Motor controller pins to Ardunio digital pins. Adjust according to your application.
 // Declare motor 1
 int enA = 3;
@@ -8,35 +9,32 @@ int enB = 10;
 int in3 = 6;
 int in4 = 9;
 
+L298n l298n(3,10,4,5,6,9);
+
 void setup() {
-  // set pin mode to output
-  pinMode(enA, OUTPUT);
-  pinMode(enB, OUTPUT);
-  pinMode(in1, OUTPUT);
-  pinMode(in2, OUTPUT);
-  pinMode(in3, OUTPUT);
-  pinMode(in4, OUTPUT);
+
 }
 
 void loop() {
+  l298n.stop();
   //forward();
   //delay(2000);
-  //stop();
+  //l298n.stop();
   //One second delay for motor to fully rest
   //delay(1000);
   //reverse();
   //delay(2000);
-  //stop();
+  //l298n.stop();
   //One second delay for motor to fully rest
   //delay(1000);
   //leftTurn();
   //delay(2000);
-  //stop();
+  //l298n.stop();
   //One second delay for motor to fully rest
   //delay(1000);
   //rightTurn();
   //delay(2000);
-  //stop();
+  //l298n.stop();
   //One second delay for motor to fully rest
   //delay(1000);
 }
@@ -65,13 +63,6 @@ void reverse(){
   digitalWrite(in4, LOW);
   //speed to 2nd motor
   analogWrite(enB, 200);
-}
-
-void stop(){
-  digitalWrite(in1, LOW);
-  digitalWrite(in2, LOW);
-  digitalWrite(in3, LOW);
-  digitalWrite(in4, LOW);
 }
 
 void leftTurn(){
